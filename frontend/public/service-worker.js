06273081cf9da259f926c614b969fc0165fc9329
Andrey <73542104+Andryxa797@users.noreply.github.com>
2023-05-01 17:48:41 +0700

    refactoring

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -7,22 +7,3 @@ self.addEventListener('install', event => {
         })
     );
 });
-
-self.addEventListener('fetch', event => {
-    if (event.request.method !== 'GET') {
-        return;
-    }
-
-    event.respondWith(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.match(event.request).then(response => {
-                const fetchPromise = fetch(event.request).then(networkResponse => {
-                    cache.put(event.request, networkResponse.clone());
-                    return networkResponse;
-                });
-
-                return response || fetchPromise;
-            });
-        })
-    );
-});
